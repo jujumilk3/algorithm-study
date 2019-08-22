1c8f632688c7f8c65df6d6189dd14c57a5355239
jujumilk3 <jujumilk3@gmail.com>
2019-08-22 16:56:49 +0900

    complete kakao 2017 1 1

--- Programmers/kakao/2017/1차/1.비밀지도.py
+++ Programmers/kakao/2017/1차/1.비밀지도.py
@@ -1,1 +1,19 @@
-print("비밀지도")
+def solution(n, arr1, arr2):
+    answer = []
+    for i, j in zip(arr1, arr2):
+        answer.append(format(i | j, 'b').zfill(n).replace('1', '#').replace('0', ' '))
+    return answer
+
+
+testcase1 = (5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28])
+testcase2 = (6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10])
+
+
+print(solution(*testcase1))
+print(solution(*testcase2))
+
+
+
+
+
+
