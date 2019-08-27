67e0f29c2ea332a473de16f9cf4ecadb39451f58
jujumilk3 <jujumilk3@gmail.com>
2019-08-27 16:19:30 +0900

    shuttle bus done

--- Programmers/kakao/2017/1차/4.셔틀버스.py
+++ Programmers/kakao/2017/1차/4.셔틀버스.py
@@ -1,49 +1,23 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17678
 
 
-def solution(run_count, term_minute, capacity, timetable):
+def solution(n, t, m, timetable):
     answer = ''
-    first_bus_arrived_time = 540
+    timetable = [int(time[:2]) * 60 + int(time[3:5]) for time in timetable]
     timetable.sort()
-    crew_count = len(timetable)
-    boarded_crew_count = 0
-
-    crew_arrive_list = list(map(lambda x: time_to_minutes_converter(x), timetable))
-    bus_arrive_list = [first_bus_arrived_time]
-    current_bus_arrived_time = bus_arrive_list[0]
-
-    for i in range(run_count - 1):
-        current_bus_arrived_time += term_minute
-        bus_arrive_list.append(current_bus_arrived_time)
-
-    for i in range(run_count):
-        current_bus_capacity = capacity
-        for j in range(crew_count):
-            if crew_arrive_list[j] <= bus_arrive_list[i]:
-                current_bus_capacity -= 1
-                boarded_crew_count += 1
-                if not current_bus_capacity:
-                    break
-        if i == run_count - 1:
-            if not current_bus_capacity:
-                answer = minutes_to_time_converter(crew_arrive_list[boarded_crew_count - 1] - 1)
-            else:
-                answer = minutes_to_time_converter(bus_arrive_list[i])
-
-    return answer
-
-
-def time_to_minutes_converter(str_time):
-    time_splitted = str_time.split(':')
-    hour = int(time_splitted[0])
-    minute = int(time_splitted[1])
-    return (hour * 60) + minute
-
-
-def minutes_to_time_converter(int_minute):
-    hour = int_minute // 60
-    minute = int_minute % 60
-    return str(hour).zfill(2) + ':' + str(minute).zfill(2)
+    for i in range(n):
+        last_time = 540 + (n - 1) * t
+        if len(timetable) < m:
+            return '%02d:%02d' % (last_time // 60, last_time % 60)
+        if i == n - 1:
+            if timetable[0] > last_time:
+                return '%02d:%02d' % (last_time // 60, last_time % 60)
+            time = timetable[m - 1] - 1
+            return '%02d:%02d' % (time // 60, time % 60)
+        for j in range(m - 1, -1, -1):
+            bus_arrive = 540 + i * t
+            if timetable[j] <= bus_arrive:
+                del timetable[j]
 
 
 # 버스의 스케쥴이 나와야 뭐가 된다.
@@ -51,6 +25,10 @@ def minutes_to_time_converter(int_minute):
 # 제공받은 문자열을 : 기준으로 하여 배열화하고 앞은 0버리고 x60하여 분을 구하고 뒤는 그냥 더해서 총 분으로 구한다
 
 # 소팅해서 먼저온 사람들을 capacity만큼 줄여나간다.
+
+# 전에 했던 거(뻘짓3)는 런타임에러 뜨고 오류 뜨고 난리나는데 이건 멀쩡하게 된다.
+# 겉으로 보이는 로직상에 큰 차이는 없는데 어떤 특정한 테스트케이스에서 오류를 뱉게 되는 것 같다.
+# 나중에 검사 한번 해보자.
 
 
 testcase1 = (1, 1, 5, ['08:00', '08:01', '08:02', '08:03'])
