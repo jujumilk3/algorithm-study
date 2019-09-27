2a8a9d6d158e4c09ac0b012412597ef92ac1b6ba
daniel lee <jujumilk3@gmail.com>
2019-09-27 22:04:04 +0900

    qwe

--- Programmers/kakao/2019/데모 테스트/1.프로그래밍1.py
+++ Programmers/kakao/2019/데모 테스트/1.프로그래밍1.py
@@ -1,26 +1,18 @@
-def solution(coordinates):
+def solution(v):
     answer = []
-    first = []
-    second = []
-    for dot in coordinates:
-        first.append(dot[0])
-        second.append(dot[1])
-
-    print(first)
-    print(second)
-    for i in range(3):
-        print("카운트는")
-        print("현재second는")
-        print(second[i])
-        print(second.count(second[i]))
-        if second.count(second[i]) % 2 != 0:
-            answer.append(second[i])
-            print("첫번째")
-            print(second[i])
-        if first.count(first[i]) % 2 != 0:
-            answer.append(first[i])
-            print(first[i])
-
+    first_stack = []
+    second_stack = []
+    for i in range(len(v)):
+        if v[i][0] not in first_stack:
+            first_stack.append(v[i][0])
+        else:
+            first_stack.remove(v[i][0])
+        if v[i][1] not in second_stack:
+            second_stack.append(v[i][1])
+        else:
+            second_stack.remove(v[i][1])
+    answer.append(*first_stack)
+    answer.append(*second_stack)
     return answer
 
 
