abc0f36b1af5cb85ddf0d856a08fa2aadfa7fdc1
daniel lee <jujumilk3@gmail.com>
2019-10-24 05:07:46 +0900

    complete get center character again

--- Programmers/level1/가운데 글자 가져오기.py
+++ Programmers/level1/가운데 글자 가져오기.py
@@ -1,10 +1,5 @@
 def solution(s):
-    answer = ''
-    if len(s) % 2 == 0:
-        answer = s[(len(s)//2) - 1: (len(s)//2) + 1]
-    else:
-        answer = s[(len(s)//2)]
-    return answer
+    return s[(len(s)-1)//2: (len(s)//2) + 1]
 
 
 print(solution("abcde"))
