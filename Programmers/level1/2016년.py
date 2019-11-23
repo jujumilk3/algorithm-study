b4add87e15bf4f7ee46355c8481284632b567fe9
daniel lee <jujumilk3@gmail.com>
2019-11-23 21:57:20 +0900

    add 2016

---
+++ Programmers/level1/2016년.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+def solution(a, b):
+    day_of_the_week = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    return day_of_the_week[datetime.datetime(2016, a, b).weekday()]
+
+
+print(solution(5, 24))
+
