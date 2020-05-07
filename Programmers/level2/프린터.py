efd54bad0f04441bfaf49e126f5d68eaa6af8a58
daniel lee <jujumilk3@gmail.com>
2020-05-08 00:01:18 +0900

    20200508

--- Programmers/level2/프린터.py
+++ Programmers/level2/프린터.py
@@ -1,11 +1,26 @@
 def solution(priorities, location):
     priority_dict = {}
     answer = 0
+    arranged = False
+
     for k, v in enumerate(priorities):
         priority_dict[k] = v
-    for v in priority_dict.items():
-        print(v[1])
     print(priority_dict)
+
+    current_priority = 0
+    current_index = 0
+    while not arranged:
+        for i in range(len(priority_dict)):
+            if i == 0:
+                current_priority = priority_dict[0]
+                current_index = 0
+            elif current_priority < priority_dict[i]:
+                current_priority = priority_dict[i]
+                current_index = priority_dict
+
+        if True:
+            arranged = True
+
     return answer
 
 
