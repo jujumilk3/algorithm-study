c59f8349ece2145094dbbad8c5638877474b374d
gyudoza <jujumilk3@gmail.com>
2021-03-09 20:27:35 +0900

    20210309-01

---
+++ Programmers/level1/소수-만들기.py
@@ -0,0 +1,7 @@
+def solution(nums):
+    answer = 0
+    return answer
+
+
+print(solution([1, 2, 3, 4]))
+print(solution([1, 2, 7, 6, 4]))
--- Programmers/level1/키패드-누르기.py
+++ Programmers/level1/키패드-누르기.py
@@ -2,7 +2,12 @@ def solution(numbers, hand):
     answer = ''
     last_left_finger = '*'
     last_right_finger = '#'
-    print('L R N')
+    numpad = {
+        1: (1, 1), 2: (2, 1), 3: (3, 1),
+        4: (1, 2), 5: (2, 2), 6: (3, 2),
+        7: (1, 3), 8: (2, 3), 9: (3, 3),
+        '*': (1, 4), 0: (2, 4), '#': (3, 4)
+    }
     for number in numbers:
         if number in [1, 4, 7]:
             answer += 'L'
@@ -11,8 +16,25 @@ def solution(numbers, hand):
             answer += 'R'
             last_right_finger = number
         else:
-            print(last_left_finger, last_right_finger, number)
-
+            left_finger_x_distance = abs(numpad[number][0] - numpad[last_left_finger][0])
+            left_finger_y_distance = abs(numpad[number][1] - numpad[last_left_finger][1])
+            left_finger_distance = left_finger_x_distance + left_finger_y_distance
+            right_finger_x_distance = abs(numpad[number][0] - numpad[last_right_finger][0])
+            right_finger_y_distance = abs(numpad[number][1] - numpad[last_right_finger][1])
+            right_finger_distance = right_finger_x_distance + right_finger_y_distance
+            if left_finger_distance < right_finger_distance:
+                answer += 'L'
+                last_left_finger = number
+            elif left_finger_distance > right_finger_distance:
+                answer += 'R'
+                last_right_finger = number
+            elif left_finger_distance == right_finger_distance:
+                if hand == 'left':
+                    answer += 'L'
+                    last_left_finger = number
+                elif hand == 'right':
+                    answer += 'R'
+                    last_right_finger = number
     return answer
 
 
