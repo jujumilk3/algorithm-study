22de3c20d9d1968a600d77c731132e3d4a4c35c5
daniel lee <jujumilk3@gmail.com>
2021-04-09 01:05:06 +0900

    20210408-02

---
+++ Programmers/level2/땅따먹기.py
@@ -0,0 +1,10 @@
+def solution(land):
+    answer = 0
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
+
+
+print(solution([[1, 2, 3, 5], [5, 6, 7, 8], [4, 3, 2, 1]]))
---
+++ Programmers/level2/타겟-넘버.py
@@ -0,0 +1,8 @@
+def solution(numbers, target):
+    answer = 0
+    return answer
+
+
+print(solution([1, 1, 1, 1, 1], 3))
+print(solution([1, 1, 1, 1, 1, 1], 4))
+print(solution([2, 4, 2, 4, 2, 4], 6))
