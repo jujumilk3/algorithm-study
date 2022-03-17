f5e617f490a9047ef98dcdce2d0bb17ed8be47f3
gyudoza <jujumilk3@gmail.com>
2022-03-17 22:57:03 +0900

    20220317

--- Programmers/level2/영어-끝말잇기.py
+++ Programmers/level2/영어-끝말잇기.py
@@ -1,10 +1,13 @@
+from math import ceil
+
+
 def solution(n, words):
     answer = [0, 0]
     already = []
     index = 0
     for word in words:
         index += 1
-        if word in already or already and already[-1][-1] != word[0]:
+        if word in already or (len(already) and already[-1][-1] != word[0]) or len(word) == 1:
             break
         else:
             already.append(word)
@@ -13,12 +16,20 @@ def solution(n, words):
             wrong = n
         else:
             wrong = index % n
-        answer = [wrong, sum(divmod(index, n))]
+        answer = [wrong, ceil(index / n)]
     return answer
 
 
-print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
-print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "trim", 'tank']))
-print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
-print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
-print(solution(1, ["hello", "hello"]))
+# print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+# print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "trim", 'tank']))
+print(solution(3, ["tank", "kick", "know",
+                   "wheel", "land", "dream",
+                   "mother", "robot", "trim",
+                   'missant', 'tank']))
+# print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+print(solution(5, ["hello", "observe", "effect", "take", "either",
+                   "recognize", "encourage", "ensure", "establish", "hang",
+                   "gather", "refer", "reference", "estimate", "executive",
+                   "executive"]))
+# print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
+# print(solution(1, ["hello", "hello"]))
--- Programmers/level2/카펫.py
+++ Programmers/level2/카펫.py
@@ -1,6 +1,10 @@
 def solution(brown, yellow):
-    answer = []
-    return answer
+    total_count = brown + yellow
+    for column in range(2, total_count + 1):
+        if (total_count / column) % 1 == 0:  # 사각형이 만들어지는 조건
+            row = total_count // column
+            if row >= column and (2 * row) + (2 * column) == brown + 4:  # 네개의 작대기를 겹치면 4개의 겹치는 사각형이 발생하니 그 숫자를 뺌.
+                return [row, column]
 
 
 print(solution(10, 2))
